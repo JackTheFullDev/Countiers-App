6a54b8105517a3497d17637491d6c74f58878ad3
Jackob <jakub.wiktor.adamczyk@gmail.com>
2023-07-23 18:44:18 +0200

    mmit
    flag-ended

--- RestCountries/src/components/FlagInfo.tsx
+++ RestCountries/src/components/FlagInfo.tsx
@@ -13,13 +13,13 @@ export const FlagInfo = ({ countryData }: { countryData: CountryDataI }) => {
 
   return (
     <section className="flex flex-col bg-white dark:bg-DarkBlueDM dark:text-white rounded items-center  ">
-      <img src={countryData[25].flags.svg} />
+      <img src={countryData[1].flags.svg} />
       <div className="flex flex-col p-5 w-full  gap-y-5 text-xs">
         <h1>{countryData[0].name} </h1>
-        <div>
-          <p>Population:{countryData[0].population}</p>
-          <p>Region:{countryData[0].region}</p>
-          <p>Capital:{countryData[0].capital}</p>
+        <div className="font-semibold">
+          <p>Population:<span>{countryData[0].population}</span></p>
+          <p>Region:<span>{countryData[0].region}</span></p>
+          <p>Capital:<span>{countryData[0].capital}</span></p>
         </div>
       </div>
     </section>
